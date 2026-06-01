7f18d4bdac6b2f882a55568cbd41e3c1ebba2f68
Khoa Luong <munm17648@gmail.com>
2026-06-01 20:31:28 +0700

    fix(auth): add missing DTO references

--- src/backend/RoadmapPlatform.Api/Controllers/AuthController.cs
+++ src/backend/RoadmapPlatform.Api/Controllers/AuthController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Mvc;
+using RoadmapPlatform.Application.DTOs.Auth;
 using RoadmapPlatform.Application.Interfaces;
 
 namespace RoadmapPlatform.Api.Controllers
